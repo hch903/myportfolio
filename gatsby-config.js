3f6e31ab99fcbcdb193ca943f91c5cf3f6f252d5
hch903 <hh2@andrew.cmu.edu>
2022-12-20 14:46:51 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `HaoChen Hsieh`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
