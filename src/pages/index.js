c59ff76a407243dfe6c32d7472f8ca0106fd994c
hch903 <hh2@andrew.cmu.edu>
2022-12-23 13:02:00 -0800

    add social icon on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Script } from "gatsby";
 import Layout from "../components/layout";
 import Navbar from "../components/navbar";
 import '../styles/global.css';
@@ -7,35 +8,71 @@ import {
   title,
   introduction,
   revealTitle,
-  revealIntro
+  revealIntro,
+  socialIcon,
+  socialIcons
 } from '../styles/index.module.css';
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <Navbar></Navbar>
-      <div className={container}>
-        <div className={title}>
-          <h1 className={revealTitle}>
-            <span>Hey, I'm HaoChen Hsieh</span>
-          </h1>
+    <>
+      <Script
+        src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+        crossorigin="anonymous"
+      ></Script>
+      <Layout>
+        <Navbar></Navbar>
+        <div className={container}>
+          <div className={title}>
+            <h1 className={revealTitle}>
+              <span>Hey, I'm HaoChen Hsieh</span>
+            </h1>
+          </div>
+          <div className={introduction}>
+            <p className={revealIntro}>
+              <span>I majored in Software Engineering and graduated from Carnegie Mellon University. </span>
+            </p>
+            <p className={revealIntro}>
+              <span>I had multiple internship experiences at AWS, Dent&Co, and Cathay United Bank.</span>
+            </p>
+            <p className={revealIntro}>
+              <span>I am familiar with backend development and full-stack development.</span>
+            </p>
+          </div>
         </div>
-        <div className={introduction}>
-          <p className={revealIntro}>
-            <span>I majored in Software Engineering and graduated from Carnegie Mellon University. </span>
-          </p>
-          <p className={revealIntro}>
-            <span>I had multiple internship experiences at AWS, Dent&Co, and Cathay United Bank.</span>
-          </p>
-          <p className={revealIntro}>
-            <span>I am familiar with backend development and full-stack development.</span>
-          </p>
+
+        <div className={socialIcons}>
+          <a
+            className={socialIcon}
+            href="https://www.linkedin.com/in/hch903/"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a className={socialIcon} href="https://github.com/hch903">
+            <i className="fab fa-github"></i>
+          </a>
+          <a className={socialIcon} href="https://twitter.com/hch903">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            className={socialIcon}
+            href="https://www.facebook.com/profile.php?id=100000240454920"
+          >
+            <i className="fab fa-facebook-f"></i>
+          </a>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>HaoChen Hsieh</title>
+export const Head = () => {
+  return (
+    <>
+      <title>HaoChen Hsieh</title>
+      <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    </>
+  );
+}
